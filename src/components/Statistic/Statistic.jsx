7ca03088c5c1a707e17488025db459193d9fd218
MishynaIra <gortenzia1987@gmail.com>
2022-12-29 12:22:50 +0200

    обновила состояние

--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -6,13 +6,32 @@ export default class Statistic extends Component {
     neutral: 0,
     bad: 0
   }
+
+  increaseGood = () => {
+    this.setState({
+        good: 3,
+      }
+      );
+  };
+  increaseNeutral = () => {
+    this.setState({
+      neutral: 2,
+      }
+    );
+  };
+  increaseBad = () => {
+    this.setState({
+      bad: 2,
+      }
+    );
+  };
   render() {
     return (
       <div>
        <h1> Please leave feedback</h1>
-        <button type='button'>Good</button>
-        <button type='button'>Neutral</button>
-        <button type='button'>Bad</button>
+        <button type='button' onClick={this.increaseGood}>Good</button>
+        <button type='button' onClick={this.increaseNeutral}>Neutral</button>
+        <button type='button' onClick={this.increaseBad}>Bad</button>
         <h2>Statistics</h2>
         <span>Good: {this.state.good}</span>
         <span>Neutral: {this.state.neutral}</span>
