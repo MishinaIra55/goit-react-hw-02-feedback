43785139b3557b6ae5075215a7c224f83a6b309e
MishynaIra <gortenzia1987@gmail.com>
2022-12-29 12:10:33 +0200

    state прописала

--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 
 export default class Statistic extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
   render() {
     return (
       <div>
        <h1> Please leave feedback</h1>
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+        <button type='button'>Good</button>
+        <button type='button'>Neutral</button>
+        <button type='button'>Bad</button>
         <h2>Statistics</h2>
-        <span>Good</span>
-        <span>Neutral</span>
-        <span>Bad</span>
+        <span>Good: {this.state.good}</span>
+        <span>Neutral: {this.state.neutral}</span>
+        <span>Bad:{this.state.bad}</span>
       </div>
     )
   }
